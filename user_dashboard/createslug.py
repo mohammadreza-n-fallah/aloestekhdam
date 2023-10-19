7865560937297c78991aae542d94ee31f56dbdc0
abol <abol@abol.com>
2023-10-19 16:11:41 +0330

    fix translate issue

--- user_dashboard/createslug.py
+++ user_dashboard/createslug.py
@@ -1,18 +1,19 @@
-from googletrans import Translator
 from datetime import datetime
+from unidecode import unidecode
 
 def ConvertSlug(text):
-    translator = Translator()
-    
     try:
-        translated = translator.translate(text, src='fa', dest='en')
-        if translated is not None:
-            translated = translator.translate(text, src='fa', dest='en')
-            slug = f"{translated.text.replace(' ', '-', -1)}-{datetime.now().year}-{datetime.now().month}-{datetime.now().day}-{datetime.now().microsecond}".lower()
-            return slug
-        else:
-            translated = translator.translate(text, src='fa', dest='en')
-            return None
+        # Transliterate the text using unidecode
+        transliterated_text = unidecode(text)
+        
+        # Replace spaces with dashes, remove other special characters, and create the slug
+        slug = '-'.join(transliterated_text.split())
+        slug = slug.lower()
+
+        # Append a timestamp to make the slug unique
+        slug += f"-{datetime.now().year}-{datetime.now().month}-{datetime.now().day}-{datetime.now().microsecond}"
+
+        return slug
     except Exception as e:
         print(f"Error in ConvertSlug: {str(e)}")
         return None
