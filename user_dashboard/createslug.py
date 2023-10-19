698fba95cf2ca69fdedcca8d5723c7666c8008a3
abol <abol@abol.com>
2023-10-19 16:00:48 +0330

    fix translate issue

--- user_dashboard/createslug.py
+++ user_dashboard/createslug.py
@@ -7,10 +7,11 @@ def ConvertSlug(text):
     try:
         translated = translator.translate(text, src='fa', dest='en')
         if translated is not None:
+            translated = translator.translate(text, src='fa', dest='en')
             slug = f"{translated.text.replace(' ', '-', -1)}-{datetime.now().year}-{datetime.now().month}-{datetime.now().day}-{datetime.now().microsecond}".lower()
             return slug
         else:
-            print ('ok')
+            translated = translator.translate(text, src='fa', dest='en')
             return None
     except Exception as e:
         print(f"Error in ConvertSlug: {str(e)}")
